fe7054e67010c2ff3e49b60ffa010d7753a2311b
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-06-09 10:48:37 +0200

    Add ability to change output directory of the archive
    To change output directory, users need to specify --outir=destination option
    Create Argument Parser class
    Update Program.cs

---
+++ VSProjectZip.Core/Parsing/ArgumentParser.cs
@@ -0,0 +1,14 @@
+﻿namespace VSProjectZip.Core.Parsing
+{
+    public class ArgumentParser
+    {
+        public IReadOnlyDictionary<string, string?> Arguments { get; private set; }
+
+        public ArgumentParser(IEnumerable<string> args)
+        {
+            Arguments = args.Select(arg => arg.Split('=', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+                .Where(array => array.Length > 0)
+                .ToDictionary(nameValueArray => nameValueArray.First(), nameValueArray => nameValueArray.LastOrDefault());
+        }
+    }
+}
--- VSProjectZip/Program.cs
+++ VSProjectZip/Program.cs
@@ -1,17 +1,21 @@
-﻿using VSProjectZip.Core.Utilities;
+﻿using VSProjectZip.Core.Parsing;
+using VSProjectZip.Core.Utilities;
 using VSProjectZip.Core.Zipping;
 
 IDirectoryCopier copier = new SkipNamesCopyUtility();
 IDirectoryZip zip = new ProjectZip(copier);
 
-var argument = args.FirstOrDefault();
-if (argument is not null)
+ArgumentParser parser = new(args.Skip(1));
+var mainArgument = args.FirstOrDefault();
+var argValuePairs = parser.Arguments;
+if (mainArgument is not null)
 {
-    var parentDirectory = new DirectoryInfo(argument).Parent;
+    var parentDirectory = new DirectoryInfo(mainArgument).Parent;
     if (parentDirectory is null)
     {
         return;
     }
 
-    zip.ZipDirectory(argument, parentDirectory.FullName);
+    string outputDirectory = argValuePairs.TryGetValue("--outdir", out var outDir) && outDir is not null ? outDir : parentDirectory.FullName;
+    zip.ZipDirectory(mainArgument, outputDirectory);
 }
