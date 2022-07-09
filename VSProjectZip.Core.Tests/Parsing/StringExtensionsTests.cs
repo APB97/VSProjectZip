6e484d122b2c2dbcc152cea35badb56d54a5379d
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-07-09 07:48:29 +0200

    Add StringExtensionsTests.cs

---
+++ VSProjectZip.Core.Tests/Parsing/StringExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using VSProjectZip.Core.Parsing;
+
+namespace VSProjectZip.Core.Tests.Parsing;
+
+[TestFixture]
+public class StringExtensionsTests
+{
+    [TestCase("")]
+    [TestCase(" ")]
+    public void ParseListArgument_ReturnsEmptySet_WhenNoItemsAreGiven(string emptyArgumentValue)
+    {
+        var hashSet = emptyArgumentValue.ParseListArgument();
+        
+        Assert.That(hashSet, Is.Empty);
+    }
+
+    [Test]
+    public void ParseListArgument_ReturnsSetWithGivenItem_WhenOneItemIsGiven()
+    {
+        string sampleValue = "sampleValue";
+        
+        var hashSet = sampleValue.ParseListArgument();
+        
+        Assert.That(hashSet, Contains.Item(sampleValue));
+    }
+
+    [Test]
+    public void ParseListArgument_ReturnsSetWithGivenItems_WhenTwoItemsAreGivenWithWhitespaces()
+    {
+        string sampleValue = "sampleValue";
+        string anotherValue = "anotherValue";
+        string listArgumentValue = $"{sampleValue} | {anotherValue}";
+
+        var hashSet = listArgumentValue.ParseListArgument();
+        
+        Assert.That(hashSet, Contains.Item(sampleValue).And.Contains(anotherValue));
+    }
+    
+    [Test]
+    public void ParseListArgument_ReturnsSetWithGivenItems_WhenTwoItemsAreGivenWithNoWhitespaces()
+    {
+        string sampleValue = "sampleValue";
+        string anotherValue = "anotherValue";
+        string listArgumentValue = $"{sampleValue}|{anotherValue}";
+
+        var hashSet = listArgumentValue.ParseListArgument();
+        
+        Assert.That(hashSet, Contains.Item(sampleValue).And.Contains(anotherValue));
+    }
+    
+}
