92981f91d3a186f445d1c88058266ec685944ff3
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-07-10 12:51:10 +0200

    Cleanup Program.cs by moving some code into CommandLineApp.cs

---
+++ VSProjectZip/CommandLineApp.cs
@@ -0,0 +1,98 @@
+﻿using VSProjectZip.Core.Parsing;
+using VSProjectZip.Core.Utilities;
+
+namespace VSProjectZip;
+
+public class CommandLineApp
+{
+    public DirectoryInfo? ReadDirectoryToZipFromFirstArgument(string[] args)
+    {
+        var mainArgument = args.FirstOrDefault();
+        if (mainArgument is not null) return new DirectoryInfo(mainArgument);
+    
+        Console.WriteLine(
+            "No arguments given. See how to use the app at https://github.com/APB97/VSProjectZip/blob/main/README.md#how-to-use");
+        return null;
+    }
+    
+    public IReadOnlyDictionary<string, string?> ReadAdditionalArguments(string[] args)
+    {
+        ArgumentParser parser = new ArgumentParser(args.Skip(1));
+        return parser.AdditionalArguments;
+    }
+    
+    public string DetermineOutputPath(IReadOnlyDictionary<string, string?> argumentValues,
+        DirectoryInfo? directoryToZip)
+    {
+        string? outputDirectory = DetermineOutputDirectory(argumentValues, directoryToZip);
+        if (outputDirectory is null) throw new ArgumentNullException(nameof(outputDirectory), "Output directory couldn't be determined.");
+        
+        string? outputName = DetermineOutputName(argumentValues, directoryToZip);
+        if (outputName is null) throw new ArgumentNullException(nameof(outputName), "Output name couldn't be determined.");
+
+        return Path.Combine(outputDirectory, outputName);
+    }
+
+    private static string? DetermineOutputName(IReadOnlyDictionary<string, string?> argumentValues, DirectoryInfo? directoryToZip)
+    {
+        return argumentValues.TryGetValue("--outname", out var outName) && outName is not null
+            ? outName
+            : directoryToZip?.Name;
+    }
+
+    private static string? DetermineOutputDirectory(IReadOnlyDictionary<string, string?> argumentValues, DirectoryInfo? directoryToZip)
+    {
+        return argumentValues.TryGetValue("--outdir", out var outDir) && outDir is not null
+            ? outDir
+            : directoryToZip?.Parent?.FullName;
+    }
+
+    public void UpdateSkippedFiles(SkipNamesCopyUtility copier, IReadOnlyDictionary<string, string?> argumentValues)
+    {
+        ClearSkippedFilesIfOverrideRequested(copier, argumentValues);
+        var skipTheseFiles = DetermineSkippedFiles(argumentValues);
+        copier.AddFiles(skipTheseFiles);
+    }
+
+    private static void ClearSkippedFilesIfOverrideRequested(SkipNamesCopyUtility copier,
+        IReadOnlyDictionary<string, string?> argumentValues)
+    {
+        bool overrideSkippedFiles = argumentValues.TryGetValue("--override-skipfiles", out _);
+        if (!overrideSkippedFiles) return;
+        
+        copier.ClearFiles();
+    }
+
+    private static IEnumerable<string> DetermineSkippedFiles(IReadOnlyDictionary<string, string?> argumentValues)
+    {
+        var skipTheseFiles =
+            argumentValues.TryGetValue("--skipfiles", out var skipFiles) && skipFiles is not null
+                ? skipFiles.ParseListArgument()
+                : Enumerable.Empty<string>();
+        
+        return skipTheseFiles;
+    }
+
+    public void UpdateSkippedDirectories(SkipNamesCopyUtility copier, IReadOnlyDictionary<string, string?> argumentValues)
+    {
+        ClearSkippedDDirectoriesIfOverrideRequested(argumentValues, copier);
+        var skipTheseDirectories = DetermineSkippedDirectories(argumentValues);
+        copier.AddDirectories(skipTheseDirectories);
+    }
+
+    private static void ClearSkippedDDirectoriesIfOverrideRequested(IReadOnlyDictionary<string, string?> argumentValues,
+        SkipNamesCopyUtility copier)
+    {
+        bool overrideSkippedDirectories = argumentValues.TryGetValue("--override-skipdirs", out _);
+        if (!overrideSkippedDirectories) return;
+        
+        copier.ClearDirectories();
+    }
+
+    private static IEnumerable<string> DetermineSkippedDirectories(IReadOnlyDictionary<string, string?> argumentValues)
+    {
+        return argumentValues.TryGetValue("--skipdirs", out var skipDirs) && skipDirs is not null
+            ? skipDirs.ParseListArgument()
+            : Enumerable.Empty<string>();
+    }
+}
--- VSProjectZip/Program.cs
+++ VSProjectZip/Program.cs
@@ -1,55 +1,26 @@
-﻿using VSProjectZip.Core.FileManagement;
-using VSProjectZip.Core.Parsing;
+﻿using VSProjectZip;
+using VSProjectZip.Core.FileManagement;
 using VSProjectZip.Core.Utilities;
 using VSProjectZip.Core.Zipping;
 
-ArgumentParser parser = new(args.Skip(1));
-var mainArgument = args.FirstOrDefault();
-var argValuePairs = parser.AdditionalArguments;
-if (mainArgument is not null)
-{
-    var directoryToZip = new DirectoryInfo(mainArgument);
-    var parentDirectory = directoryToZip.Parent;
-    if (parentDirectory is null)
-    {
-        return;
-    }
+var commandLine = new CommandLineApp();
+var directoryToZip = commandLine.ReadDirectoryToZipFromFirstArgument(args);
+var argumentValues = commandLine.ReadAdditionalArguments(args);
+var outputPath = commandLine.DetermineOutputPath(argumentValues, directoryToZip);
 
-    string outputDirectory = argValuePairs.TryGetValue("--outdir", out var outDir) && outDir is not null ? outDir : parentDirectory.FullName;
-    string outputName = argValuePairs.TryGetValue("--outname", out var outName) && outName is not null ? outName : $"{directoryToZip.Name}.zip";
-    string outputPath = Path.Combine(outputDirectory, outputName);
+IDirectory directoryImplementation = new DirectoryImplementation();
+IPath pathImplementation = new PathImplementation();
+IFile fileImplementation = new FileImplementation();
+SkipNamesCopyUtility copier = new(directoryImplementation, fileImplementation, pathImplementation);
 
-    bool overrideSkippedDirectories = argValuePairs.TryGetValue("--override-skipdirs", out _);
-    bool overrideSkippedFiles = argValuePairs.TryGetValue("--override-skipfiles", out _);
-    var skipTheseDirectories =
-        argValuePairs.TryGetValue("--skipdirs", out var skipDirs) && skipDirs is not null 
-        ? skipDirs.ParseListArgument()
-        : Enumerable.Empty<string>();
-    var skipTheseFiles =
-        argValuePairs.TryGetValue("--skipfiles", out var skipFiles) && skipFiles is not null
-        ? skipFiles.ParseListArgument()
-        : Enumerable.Empty<string>();
-    
-    IDirectory directoryImplementation = new DirectoryImplementation();
-    IPath pathImplementation = new PathImplementation();
-    IFile fileImplementation = new FileImplementation();
-    SkipNamesCopyUtility copier = new(directoryImplementation, fileImplementation, pathImplementation);
-    var rootPathName = directoryToZip.Name;
+commandLine.UpdateSkippedFiles(copier, argumentValues);
+commandLine.UpdateSkippedDirectories(copier, argumentValues);
 
-    if (overrideSkippedDirectories)
-    {
-        copier.ClearDirectories();
-    }
+if (directoryToZip is null) return;
+var rootPathName = directoryToZip.Name;
 
-    if (overrideSkippedFiles)
-    {
-        copier.ClearFiles();
-    }
-
-    copier.AddDirectories(skipTheseDirectories);
-    copier.AddFiles(skipTheseFiles);
-    string temporaryPath = pathImplementation.Combine(AppContext.BaseDirectory, TemporaryLocation.TempLocationName, rootPathName);
-    ITemporaryLocation temp = new TemporaryLocation(directoryImplementation, temporaryPath);
-    IDirectoryZip zip = new ProjectZip(copier, temp, fileImplementation, new ZipFileImplementation());
-    zip.ZipDirectory(mainArgument, outputPath);
-}
+string temporaryPath =
+    pathImplementation.Combine(AppContext.BaseDirectory, TemporaryLocation.TempLocationName, rootPathName);
+ITemporaryLocation temp = new TemporaryLocation(directoryImplementation, temporaryPath);
+IDirectoryZip zip = new ProjectZip(copier, temp, fileImplementation, new ZipFileImplementation());
+zip.ZipDirectory(directoryToZip.FullName, outputPath);
