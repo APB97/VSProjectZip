4ebd5f55888310b568d04c4fb4ef7ec80f93c1bb
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-07-12 21:30:31 +0200

    Add first failing test for ConsoleLogger

---
+++ VSProjectZip.Core.Tests/Logging/ConsoleOutputTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using VSProjectZip.Core.Logging;
+using VSProjectZip.Core.StandardIO.Output;
+
+namespace VSProjectZip.Core.Tests.Logging;
+
+[TestFixture]
+public class ConsoleLoggerTests
+{
+    [Test]
+    public void ConsoleLogger_Info_SetsColorToWhite()
+    {
+        Mock<IConsoleOutput> consoleOutputMock = new Mock<IConsoleOutput>();
+        ConsoleLogger logger = new ConsoleLogger();
+        
+        logger.Info("Sample text");
+        
+        consoleOutputMock.VerifySet(output => output.Color = ConsoleColor.White, Times.AtLeastOnce);
+    }
+}
