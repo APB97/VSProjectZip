8cbfd831eccae512f2bf19507624b5e7dcb31b3f
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-07-12 21:30:04 +0200

    Add ILogger.cs and unimplemented methods in ConsoleLogger.cs

---
+++ VSProjectZip.Core/Logging/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿namespace VSProjectZip.Core.Logging;
+
+public class ConsoleLogger : ILogger
+{
+    public void Info(string value)
+    {
+    }
+
+    public void Warn(string value)
+    {
+    }
+
+    public void Error(string value)
+    {
+    }
+}
---
+++ VSProjectZip.Core/Logging/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace VSProjectZip.Core.Logging;
+
+public interface ILogger
+{
+    void Info(string value);
+    void Warn(string value);
+    void Error(string value);
+}
