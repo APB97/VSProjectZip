79998d3a1bbb3ba3967dcbfa9b7e3a5e8ecaecd7
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2022-07-13 09:29:47 +0200

    Upddate CommandLineApp.cs so that test passes

--- VSProjectZip.Core/Parsing/CommandLineApp.cs
+++ VSProjectZip.Core/Parsing/CommandLineApp.cs
@@ -5,8 +5,11 @@ namespace VSProjectZip.Core.Parsing;
 
 public class CommandLineApp
 {
+    private readonly ILogger _logger;
+
     public CommandLineApp(ILogger logger)
     {
+        _logger = logger;
     }
 
     public DirectoryInfo? ReadDirectoryToZipFromFirstArgument(string[] args)
@@ -14,7 +17,7 @@ public class CommandLineApp
         var mainArgument = args.FirstOrDefault();
         if (mainArgument is not null) return new DirectoryInfo(mainArgument);
     
-        Console.WriteLine(
+        _logger.Info(
             "No arguments given. See how to use the app at https://github.com/APB97/VSProjectZip/blob/main/README.md#how-to-use");
         return null;
     }
