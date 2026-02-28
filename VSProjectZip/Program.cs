7444d12b08aa42a1861192c2ccd3563b5dc63fe3
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2026-02-28 08:31:17 +0100

    Catch Exception in Program.cs, use return <CODE> and print error message

--- VSProjectZip/Program.cs
+++ VSProjectZip/Program.cs
@@ -1,25 +1,38 @@
 ﻿using VSProjectZip.Core.FileManagement;
+using VSProjectZip.Core.Logging;
 using VSProjectZip.Core.Parsing;
+using VSProjectZip.Core.StandardIO.Output;
 using VSProjectZip.Core.Utilities;
 using VSProjectZip.Core.Zipping;
 
-var directoryToZip = new DirectoryInfo(args.Last());
-var arguments = new ArgumentParser(args.SkipLast(1));
-var commandLine = new CommandLineApp(directoryToZip, arguments);
-var outputPath = commandLine.DetermineOutputPath();
+var consoleLogger = new ConsoleLogger(ConsoleOutput.Instance);
 
-var directoryImplementation = new DirectoryImplementation();
-var pathImplementation = new PathImplementation();
-var fileImplementation = new FileImplementation();
-var fileSystem = new FileSystem(directoryImplementation, fileImplementation, pathImplementation);
-var copier = new SkipNamesUtility(fileSystem);
-var zipFileImplementation = new ZipFileImplementation();
+try
+{
+	var directoryToZip = new DirectoryInfo(args.Last());
+	var arguments = new ArgumentParser(args.SkipLast(1));
+	var commandLine = new CommandLineApp(directoryToZip, arguments);
+	var outputPath = commandLine.DetermineOutputPath();
 
-var skippedItems = new SkippedItemsUpdater(copier);
-skippedItems.UpdateSkippedFiles(arguments.AdditionalArguments);
-skippedItems.UpdateSkippedDirectories(arguments.AdditionalArguments);
+	var directoryImplementation = new DirectoryImplementation();
+	var pathImplementation = new PathImplementation();
+	var fileImplementation = new FileImplementation();
+	var fileSystem = new FileSystem(directoryImplementation, fileImplementation, pathImplementation);
+	var copier = new SkipNamesUtility(fileSystem);
+	var zipFileImplementation = new ZipFileImplementation();
 
-var zip = new ProjectZip(copier, zipFileImplementation, fileImplementation, directoryImplementation,
-    arguments.AdditionalArguments.ContainsKey(ArgumentCollection.Force));
+	var skippedItems = new SkippedItemsUpdater(copier);
+	skippedItems.UpdateSkippedFiles(arguments.AdditionalArguments);
+	skippedItems.UpdateSkippedDirectories(arguments.AdditionalArguments);
 
-await zip.ZipDirectoryAsync(directoryToZip.FullName, outputPath);
+	var zip = new ProjectZip(copier, zipFileImplementation, fileImplementation, directoryImplementation,
+		arguments.AdditionalArguments.ContainsKey(ArgumentCollection.Force));
+
+	await zip.ZipDirectoryAsync(directoryToZip.FullName, outputPath);
+	return 0;
+}
+catch (Exception ex)
+{
+	consoleLogger.Error(ex.Message);
+	return 1;
+}
